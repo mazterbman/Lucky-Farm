a8407f648106c7e0a3c61aa7c163f6e742dd7b62
mazterbman <dekster2468zz@gmail.com>
2026-04-20 05:59:52 +0300

    Add
    Create MobPathController

---
+++ Assets/Game/Scripts/Mobs/MobPathController.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Game.Scripts.Mobs
+{
+    public class MobPathController : MonoBehaviour
+    {
+        [Header("References")] [SerializeField]
+        private NavMeshAgent _navMeshAgent = null;
+
+        private void Start()
+        {
+            
+        }
+    }
+}
