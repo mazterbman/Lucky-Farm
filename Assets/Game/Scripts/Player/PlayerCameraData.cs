c835ca406f3b6c783df76e6692d8f8a2cb57499e
mazterbman <dekster2468zz@gmail.com>
2026-04-20 08:11:12 +0300

    Add
    Add PlayerCameraData - add some InputActionReferences

---
+++ Assets/Game/Scripts/Player/PlayerCameraData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Scripts.Player
+{
+    [Serializable]
+    public class PlayerCameraData
+    {
+        public CinemachineSplineDolly SplineDolly = null;
+        public Camera Camera = null;
+        
+        [Space]
+        public InputActionReference ScrollAction = null;
+        public InputActionReference RotationActionLeft = null;
+        public InputActionReference RotationActionRight = null;
+    }
+}
