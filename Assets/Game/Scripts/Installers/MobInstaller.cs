b4ca19232db1147ad0974c3dfcfd001bfee1ad11
mazterbman <dekster2468zz@gmail.com>
2026-04-20 06:06:03 +0300

    Add
    Create MobInstaller

---
+++ Assets/Game/Scripts/Installers/MobInstaller.cs
@@ -0,0 +1,17 @@
+using Unity.AI.Navigation;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Scripts.Installers
+{
+    public class MobInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private NavMeshSurface _meshSurface = null;
+    
+        public override void InstallBindings()
+        {
+            Container.Bind<NavMeshSurface>().FromInstance(_meshSurface).AsSingle();
+        }
+    }
+}
